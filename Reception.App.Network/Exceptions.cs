ced938a8d50d904a5f8c9928833628a1d48d1d17
kirichenec <kirichenecigor@gmail.com>
2019-12-13 12:47:32 +0300

    [~] Full ISerializable implementation for NotFoundException and QueryException

--- Reception.App.Network/Exceptions.cs
+++ Reception.App.Network/Exceptions.cs
@@ -1,16 +1,59 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace Reception.App.Network.Exceptions
 {
+    [Serializable]
     public class NotFoundException<T> : Exception
     {
-        public T ExceptionObject { get; set; }
+        public T ExceptionObject { get; }
+
+        public NotFoundException() : base() { }
 
         public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
+        public NotFoundException(string message, T exceptionObject) : base(message)
+        {
+            ExceptionObject = exceptionObject;
+        }
+
+        public NotFoundException(string message, T exceptionObject, Exception innerException) : base(message, innerException)
+        {
+            ExceptionObject = exceptionObject;
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            ExceptionObject = (T)info.GetValue(nameof(ExceptionObject), typeof(T));
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException(nameof(info));
+            }
+
+            info.AddValue(nameof(ExceptionObject), ExceptionObject);
+
+            base.GetObjectData(info, context);
+        }
     }
 
+    [Serializable]
     public class QueryException : Exception
     {
+        public QueryException() { }
+
         public QueryException(string message) : base(message) { }
+
+        public QueryException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected QueryException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
