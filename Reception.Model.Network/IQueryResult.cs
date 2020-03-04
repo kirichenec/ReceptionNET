f4ea7f6b770f8e917a5633573efb67100ea7ed67
kirichenec <kirichenecigor@gmail.com>
2020-03-04 21:49:37 +0300

    All: [~] Default DataType value by generic type

--- Reception.Model.Network/IQueryResult.cs
+++ Reception.Model.Network/IQueryResult.cs
@@ -1,8 +1,11 @@
-﻿namespace Reception.Model.Network
+﻿using System;
+
+namespace Reception.Model.Network
 {
     public interface IQueryResult<T>
     {
         public T Data { get; set; }
+        public Type DataType { get; set; }
         public ErrorCode ErrorCode { get; set; }
         public string ErrorMessage { get; set; }
     }
--- Reception.Model.Network/QueryResult.cs
+++ Reception.Model.Network/QueryResult.cs
@@ -5,7 +5,7 @@ namespace Reception.Model.Network
     public class QueryResult<T> : IQueryResult<T>
     {
         public T Data { get; set; }
-        public Type DataType { get; set; }
+        public Type DataType { get; set; } = typeof(T);
         public ErrorCode ErrorCode { get; set; }
         public string ErrorMessage { get; set; }
     }
