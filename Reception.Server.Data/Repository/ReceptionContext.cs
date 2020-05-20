355bc556a03c543ee5f85121f9f5c6138743a0de
kirichenec <kirichenecigor@gmail.com>
2020-05-20 17:57:21 +0300

    All: [U] Some PackageReferences
    Server:
    [+] Sql logging

--- Reception.Server.Data/Repository/ReceptionContext.cs
+++ Reception.Server.Data/Repository/ReceptionContext.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Reception.Model.Dto;
 
 namespace Reception.Server.Data.Repository
 {
     public class ReceptionContext : DbContext
     {
+        public static readonly ILoggerFactory ReceptionLoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        public DbSet<PersonDto> Persons { get; set; }
+        public DbSet<PostDto> Posts { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = $"{nameof(Reception)}.db" };
             var connectionString = connectionStringBuilder.ToString();
             var connection = new SqliteConnection(connectionString);
 
-            optionsBuilder.UseSqlite(connection);
+            optionsBuilder
+                .UseLoggerFactory(ReceptionLoggerFactory)
+                .UseSqlite(connection);
         }
-
-        public DbSet<PersonDto> Persons { get; set; }
-        public DbSet<PostDto> Posts { get; set; }
     }
 }
