0ee6adf70c9f7628c4b3b5abd4946fc321836c7e
kirichenec <kirichenecigor@gmail.com>
2022-01-16 00:36:20 +0300

    feat: AddPhotoLink migration step added

--- src/Reception.Server.Data/Migrations/20220115213329_AddPhotoLink.Designer.cs
+++ src/Reception.Server.Data/Migrations/20220115213329_AddPhotoLink.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reception.Server.Data.Repository;
 
@@ -10,9 +11,10 @@ using Reception.Server.Data.Repository;
 namespace Reception.Server.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220115213329_AddPhotoLink")]
+    partial class AddPhotoLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
