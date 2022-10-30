be4a65dc2227dbb835589fd63c12d4cf43503c33
kirichenec <kirichenecigor@gmail.com>
2022-10-30 22:23:40 +0300

    feat: App NetworkServise response reworked

--- src/Reception.App.Network/Server/NetworkService.cs
+++ src/Reception.App.Network/Server/NetworkService.cs
@@ -2,7 +2,9 @@
 using Reception.App.Network.Auth;
 using Reception.App.Network.Exceptions;
 using Reception.Model.Network;
+using RestSharp;
 using Splat;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,18 +25,12 @@ namespace Reception.App.Network.Server
         public async Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default)
         {
             var response = await Core.ExecuteGetTaskAsync(
-                baseUrl: $"{_serverPath}/api/",
+                baseUrl: $"{_serverPath}/api",
                 methodUri: $"{typeof(T).Name}/{id}",
                 headers: GetDefaultHeaders(),
                 cancellationToken);
 
-            if (response.IsSuccessful)
-            {
-                var content = JsonConvert.DeserializeObject<QueryResult<T>>(response.Content);
-                return content.Data;
-            }
-
-            throw new QueryException(response.StatusDescription, response.StatusCode);
+            return ValidateAndGetResponseData<T>(response);
         }
 
         public async Task<IEnumerable<T>> GetByIdsAsync(IEnumerable<int> ids, CancellationToken cancellationToken = default)
@@ -46,32 +42,30 @@ namespace Reception.App.Network.Server
                 headers: GetDefaultHeaders(),
                 cancellationToken);
 
-            if (response.IsSuccessful)
-            {
-                var content = JsonConvert.DeserializeObject<QueryResult<IEnumerable<T>>>(response.Content);
-                return content.Data;
-            }
-
-            throw new QueryException(response.StatusDescription, response.StatusCode);
+            return ValidateAndGetResponseData<IEnumerable<T>>(response);
         }
 
         public async Task<IEnumerable<T>> SearchAsync(string searchText, CancellationToken cancellationToken = default)
         {
             var response = await Core.ExecuteGetTaskAsync(
-                baseUrl: $"{_serverPath}/api/",
+                baseUrl: $"{_serverPath}/api",
                 methodUri: $"{typeof(T).Name}?searchText={searchText}",
                 headers: GetDefaultHeaders(),
                 cancellationToken);
 
-            if (response.IsSuccessful)
-            {
-                var content = JsonConvert.DeserializeObject<QueryResult<IEnumerable<T>>>(response.Content);
-                return content.Data;
-            }
-
-            throw new QueryException(response.StatusDescription, response.StatusCode);
+            return ValidateAndGetResponseData<IEnumerable<T>>(response);
         }
 
         private (string, string)[] GetDefaultHeaders() => _authService.GetDefaultHeaders();
+
+        private static TOut ValidateAndGetResponseData<TOut>(RestResponse response)
+        {
+            return response.ResponseStatus switch
+            {
+                ResponseStatus.Completed => JsonConvert.DeserializeObject<QueryResult<TOut>>(response.Content).Data,
+                ResponseStatus.Aborted => throw new OperationCanceledException(response.ErrorMessage),
+                _ => throw new QueryException(response.StatusDescription, response.StatusCode),
+            };
+        }
     }
 }
