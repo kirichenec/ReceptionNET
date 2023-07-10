d32b1ac7a0b6c213dab26a6721788b256c1e10e0
kirichenec <kirichenecigor@gmail.com>
2023-07-10 04:11:32 +0300

    feat: Localizer unit test

---
+++ tests/app/Reception.App.Localization.Test/LocalizationFixture.cs
@@ -0,0 +1,35 @@
+﻿using Reception.App.Localization.Languages;
+using System.Globalization;
+using System.Resources;
+
+namespace Reception.App.Localization.Test
+{
+    public class LocalizationFixture
+    {
+        private readonly ResourceManager _resources;
+
+        public LocalizationFixture()
+        {
+            _resources = new ResourceManager(typeof(Language));
+        }
+
+        public IEnumerable<string> GetAllKeys()
+        {
+            var resourceSet = _resources
+                .GetResourceSet(CultureInfo.CurrentCulture, true, true)!
+                .GetEnumerator();
+            while (resourceSet.MoveNext())
+            {
+                if (resourceSet.Key != null && !string.IsNullOrEmpty(resourceSet.Key.ToString()))
+                {
+                    yield return resourceSet.Key.ToString()!;
+                }
+            }
+        }
+
+        public ResourceManager GetResourceManager()
+        {
+            return _resources;
+        }
+    }
+}
---
+++ tests/app/Reception.App.Localization.Test/LocalizationTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Reception.App.Localization.Test
+{
+    public class LocalizationTests : IClassFixture<LocalizationFixture>
+    {
+        private readonly LocalizationFixture _localizationFixture;
+
+        public LocalizationTests(LocalizationFixture localizationFixture)
+        {
+            _localizationFixture = localizationFixture;
+        }
+
+        [Fact]
+        public void Localizer_Resources_AllKeysHasValues()
+        {
+            // Arrange
+            var resourceKeys = _localizationFixture.GetAllKeys();
+            var languageKeys = Localizer.Languages
+                .Where(lang => !string.IsNullOrEmpty(lang.Value))
+                .Select(lang => lang.Key)
+                .ToList();
+
+            // Act
+            var allResourceValues = new Dictionary<string, List<string>>();
+            foreach (var key in languageKeys)
+            {
+                Localizer.Instance.SetLanguage(key);
+                allResourceValues.Add(
+                    key, resourceKeys.Select(rKey => Localizer.Instance[rKey]).ToList()
+                    );
+            }
+
+            // Assert
+            Assert.True(allResourceValues.Values.All(l => l.Count == allResourceValues.Values.First().Count));
+        }
+    }
+}
