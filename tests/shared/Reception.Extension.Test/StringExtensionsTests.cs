3ddc82a533cdd842dee0035a53ac47efa1c902c7
kirichenec <kirichenecigor@gmail.com>
2023-07-11 03:05:14 +0300

    feat: StringExtensions CutEndBy tests

--- tests/shared/Reception.Extension.Test/StringExtensionsTests.cs
+++ tests/shared/Reception.Extension.Test/StringExtensionsTests.cs
@@ -26,5 +26,43 @@ namespace Reception.Extension.Test
             // Assert
             result.Should().Be(expectedResult);
         }
+
+        [Theory]
+        [InlineData("", null)]
+        [InlineData(null, "")]
+        [InlineData(null, null)]
+        public void StringExtensions_CutEndBy_NotThrowing(string sourceValue, string cuttedValue)
+        {
+            // Act
+            var act = () => sourceValue.CutEndBy(cuttedValue);
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
+        [Theory]
+        // Any null returns source value
+        [InlineData("", null, "")]
+        [InlineData(null, "", null)]
+        [InlineData(null, null, null)]
+        [InlineData("value", null, "value")]
+        // Source value contains cutted at the end
+        [InlineData("value", "ue", "val")]
+        [InlineData("value", "value", "")]
+        [InlineData("valuevalue", "value", "value")]
+        // Source value contains cutted not at the end
+        [InlineData("value", "val", "value")]
+        // Source value doesn't contain the cut value
+        [InlineData("value", "valuevalue", "value")]
+        [InlineData("value", "123", "value")]
+        public void StringExtensions_CutEndBy_ReturnsExpectedString(
+            string sourceValue, string cuttedValue, string expectedResult)
+        {
+            // Arrange
+            var result = sourceValue.CutEndBy(cuttedValue);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
     }
 }
