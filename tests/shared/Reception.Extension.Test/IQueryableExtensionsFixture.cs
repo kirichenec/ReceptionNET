4aaa477e2a9dae3ab10329fa08fb932a9939c2f5
kirichenec <kirichenecigor@gmail.com>
2023-07-12 05:04:33 +0300

    feat: IQueryableExtensions Paged tests

---
+++ tests/shared/Reception.Extension.Test/IQueryableExtensionsFixture.cs
@@ -0,0 +1,15 @@
+﻿namespace Reception.Extension.Test
+{
+    public static class IQueryableExtensionsFixture
+    {
+        public static IQueryable<string> GetData(uint count)
+        {
+            var result = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = $"value{i + 1}";
+            }
+            return result.AsQueryable();
+        }
+    }
+}
