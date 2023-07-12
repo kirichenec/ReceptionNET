f2a02d25e5db6547dd1f0e3e560667f94386f66d
kirichenec <kirichenecigor@gmail.com>
2023-07-13 02:59:21 +0300

    feat: ConfigurationManagerExtensions GetAppSettingsParam tests

---
+++ tests/shared/Reception.Extension.Test/ConfigurationManagerExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+
+namespace Reception.Extension.Test
+{
+    public class ConfigurationManagerExtensionsTests
+    {
+        [Fact]
+        public void ConfigurationManagerExtensions_GetAppSettingsParam_ParameterNameIsNull_ShouldNotThrown()
+        {
+            // Act
+            var act = () => ConfigurationManagerExtensions.GetAppSettingsParam(null);
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("IsBoss")]
+        public void ConfigurationManagerExtensions_GetAppSettingsParam_ReturnsExpected(
+            string parameterName)
+        {
+            // Arrange
+            ConfigurationManager.AppSettings[parameterName] = "value";
+
+            // Act
+            var act = () => ConfigurationManagerExtensions.GetAppSettingsParam(parameterName);
+
+            // Assert
+            act().Should().Be(ConfigurationManager.AppSettings[parameterName]);
+        }
+    }
+}
