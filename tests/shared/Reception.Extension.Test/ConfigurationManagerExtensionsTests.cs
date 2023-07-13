b952550003a21da03492edf13b0074a5240cfa60
kirichenec <kirichenecigor@gmail.com>
2023-07-13 05:39:22 +0300

    feat: ConfigurationManagerExtensions GetSection tests

--- tests/shared/Reception.Extension.Test/ConfigurationManagerExtensionsTests.cs
+++ tests/shared/Reception.Extension.Test/ConfigurationManagerExtensionsTests.cs
@@ -1,7 +1,11 @@
-﻿using System.Configuration;
+﻿using Reception.App.Service;
+using System.Configuration;
 
 namespace Reception.Extension.Test
 {
+    /// <summary>
+    /// Class uses testhost.dll.config
+    /// </summary>
     public class ConfigurationManagerExtensionsTests
     {
         [Fact]
@@ -17,17 +21,47 @@ namespace Reception.Extension.Test
         [Theory]
         [InlineData(null)]
         [InlineData("IsBoss")]
-        public void ConfigurationManagerExtensions_GetAppSettingsParam_ReturnsExpected(
-            string parameterName)
+        public void ConfigurationManagerExtensions_GetAppSettingsParam_ReturnsExpected(string parameterName)
         {
             // Arrange
-            ConfigurationManager.AppSettings[parameterName] = "value";
+            var expectedResult = ConfigurationManager.AppSettings[parameterName];
 
             // Act
             var act = () => ConfigurationManagerExtensions.GetAppSettingsParam(parameterName);
 
             // Assert
-            act().Should().Be(ConfigurationManager.AppSettings[parameterName]);
+            act().Should().Be(expectedResult);
+        }
+
+
+        [Fact]
+        public void ConfigurationManagerExtensions_GetSection_ParameterNameIsNull_ShouldNotThrown()
+        {
+            // Act
+            var act = () => ConfigurationManagerExtensions.GetSection<object>(null);
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData("tokenSettings")]
+        public void ConfigurationManagerExtensions_GetSection_ReturnsExpectedOfTypeTokenSection(string sectionName)
+        {
+            ConfigurationManagerExtensions_GetSection_ReturnsExpectedOfType<TokenSection>(sectionName);
+        }
+
+        private static void ConfigurationManagerExtensions_GetSection_ReturnsExpectedOfType<T>(string sectionName)
+        {
+            // Arrange
+            var expectedResult = (T)ConfigurationManager.GetSection(sectionName);
+
+            // Act
+            var act = () => ConfigurationManagerExtensions.GetSection<T>(sectionName);
+
+            // Assert
+            act().Should().Be(expectedResult);
+            act().Should().NotBeNull();
         }
     }
 }
