febb3a0c22a38ec8209cabc4edd5167102f9e9e5
kirichenec <kirichenecigor@gmail.com>
2023-07-13 09:07:59 +0300

    feat: ConfigurationManagerExtensions UpdateAppSettingsParam tests

--- tests/shared/Reception.Extension.Test/ConfigurationManagerExtensionsTests.cs
+++ tests/shared/Reception.Extension.Test/ConfigurationManagerExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using Reception.App.Service;
+using System.Collections.ObjectModel;
 using System.Configuration;
 
 namespace Reception.Extension.Test
@@ -8,6 +9,25 @@ namespace Reception.Extension.Test
     /// </summary>
     public class ConfigurationManagerExtensionsTests
     {
+        private readonly ReadOnlyDictionary<string, string> AppSettingsInitData =
+            new Dictionary<string, string>
+            {
+                { "IsBoss", "False" },
+                { "IsSystemTheme", "True" },
+                { "PingDelay", "15" },
+            }.AsReadOnly();
+
+
+        public ConfigurationManagerExtensionsTests()
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings.Clear();
+            AppSettingsInitData.ForEach(x => config.AppSettings.Settings.Add(x.Key, x.Value));
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+
         [Fact]
         public void ConfigurationManagerExtensions_GetAppSettingsParam_ParameterNameIsNull_ShouldNotThrown()
         {
@@ -62,6 +82,52 @@ namespace Reception.Extension.Test
             // Assert
             act().Should().Be(expectedResult);
             act().Should().NotBeNull();
+        }
+
+
+        [Fact]
+        public void ConfigurationManagerExtensions_UpdateAppSettingsParam_SourceValueIsNull_ShouldThrow()
+        {
+            // Arrange
+            string value = null;
+
+            // Act
+            var act = () => value.UpdateAppSettingsParam("testParam");
+
+            // Assert
+            act.Should().Throw<NullReferenceException>();
+        }
+
+        [Fact]
+        public void ConfigurationManagerExtensions_UpdateAppSettingsParam_ParamNameIsNull_ShouldThrow()
+        {
+            // Arrange
+            string value = "testValue";
+
+            // Act
+            var act = () => value.UpdateAppSettingsParam(null);
+
+            // Assert
+            act.Should().Throw<ConfigurationErrorsException>();
+        }
+
+        [Theory]
+        [InlineData(0, 1, "TestInt")]
+        [InlineData(0d, 1.1d, "TestDouble")]
+        [InlineData(0f, -1.1f, "TestSingle")]
+        [InlineData("InitTestValue", "TestValue", "TestString")]
+        [InlineData(false, true, "TestBool")]
+        public void ConfigurationManagerExtensions_UpdateAppSettingsParam_SavedSuccessfully<T>(
+            T initValue, T value, string parameterName)
+        {
+            // Arrange
+            ConfigurationManager.AppSettings[parameterName] = initValue.ToString();
+            value.UpdateAppSettingsParam(parameterName);
+            var result = ConfigurationManager.AppSettings[parameterName];
+
+            // Assert
+            result.Should().NotBe(initValue.ToString());
+            result.Should().Be(value.ToString());
         }
     }
 }
