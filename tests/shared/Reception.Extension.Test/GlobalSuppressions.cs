7f1213bacee48239289f161f1cb0b8e00bf54b5e
kirichenec <kirichenecigor@gmail.com>
2024-07-02 13:51:18 +0300

    feat: Converters tests

--- tests/shared/Reception.Extension.Test/GlobalSuppressions.cs
+++ tests/shared/Reception.Extension.Test/GlobalSuppressions.cs
@@ -7,9 +7,13 @@ using System.Diagnostics.CodeAnalysis;
 
 // Ignore local functions for tests
 [assembly: SuppressMessage("Style", "IDE0039:Use local function", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Reception.Extension.Test")]
+[assembly: SuppressMessage("Style", "IDE0039:Use local function", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Reception.Extension.Converters.Test")]
 // Ignore array parameters for tests
 [assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Reception.Extension.Test")]
 [assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Reception.Extension.Converters.Test")]
 // Bug still not fixed
 // https://github.com/xunit/xunit/issues/2075
 [assembly: SuppressMessage("Usage", "xUnit1010:The value is not convertible to the method parameter type", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Reception.Extension.Test")]
+// Ignore ISerializable
+[assembly: SuppressMessage("Usage", "xUnit1042:The member referenced by the MemberData attribute returns untyped data rows", Justification = "<Pending>", Scope = "type", Target = "~T:Reception.Extension.Converters.Test.NewtonsoftConvertersTests")]
+[assembly: SuppressMessage("Usage", "xUnit1045:Avoid using TheoryData type arguments that might not be serializable", Justification = "<Pending>", Scope = "type", Target = "~T:Reception.Extension.Converters.Test.NewtonsoftConvertersTests")]
---
+++ tests/shared/Reception.Extension.Test/Tests/FilePathToByteArrayConverterTests.cs
@@ -0,0 +1,86 @@
+﻿using Reception.Extension.Converters;
+using Reception.Extension.Test.Fixture;
+
+namespace Reception.Extension.Test.Tests;
+
+public class FilePathToByteArrayConverterTests
+{
+    [Fact]
+    public async Task FilePathToByteArrayConverter_GetFileBytesByPathAsync_FilePathIsNull_ShouldThrow()
+    {
+        // arrange
+        string filePath = null;
+
+        // act
+        var act = async () => await filePath.GetFileBytesByPathAsync();
+
+        // assert
+        await act.Should().ThrowAsync<ArgumentNullException>();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task FilePathToByteArrayConverter_GetFileBytesByPathAsync_FilePathIsEmptyOrWhiteSpace_ShouldThrow(string filePath)
+    {
+        // act
+        var act = async () => await filePath.GetFileBytesByPathAsync();
+
+        // assert
+        await act.Should().ThrowAsync<ArgumentException>();
+    }
+
+    [Fact]
+    public async Task FilePathToByteArrayConverter_GetFileBytesByPathAsync_FilePathOfNonExistedFile_ShouldThrow()
+    {
+        // arrange
+        string filePath = "Name.jpgxc";
+
+        // act
+        var act = async () => await filePath.GetFileBytesByPathAsync();
+
+        // assert
+        await act.Should().ThrowAsync<FileNotFoundException>();
+    }
+
+    [Fact]
+    public async Task FilePathToByteArrayConverter_GetFileBytesByPathAsync_FilePathOfNonExistedFolder_ShouldThrow()
+    {
+        // arrange
+        string filePath = "randomUnsupported\\Name";
+
+        // act
+        var act = async () => await filePath.GetFileBytesByPathAsync();
+
+        // assert
+        await act.Should().ThrowAsync<DirectoryNotFoundException>();
+    }
+
+    [Fact]
+    public async Task FilePathToByteArrayConverter_GetFileBytesByPathAsync_IncorrectFilePath_ShouldThrow()
+    {
+        // arrange
+        string filePath = "fileName .jpg";
+
+        // act
+        var act = async () => await filePath.GetFileBytesByPathAsync();
+
+        // assert
+        await act.Should().ThrowAsync<IOException>();
+    }
+
+    [Fact]
+    public async Task FilePathToByteArrayConverter_GetFileBytesByPathAsync_ReturnExpected()
+    {
+        // arrange
+        using var tempFile = new TemporaryFile();
+        var filePath = tempFile.FilePath;
+        File.SetAttributes(filePath, File.GetAttributes(filePath) | FileAttributes.Temporary);
+
+        // act
+        var file = await filePath.GetFileBytesByPathAsync();
+
+        // Assert
+        file.Should().BeEquivalentTo(Array.Empty<byte>());
+    }
+}
