6347680b59e2c90e30e81032166ded4d2c99aa85
kirichenec <kirichenecigor@gmail.com>
2026-03-20 00:07:36 +0300

    fix: S3343 suppression for ShowErrorAction

---
+++ src/Reception.App/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Bug", "S3343:Caller information parameters should come at the end of the parameter list",
+    Justification = "CallerMemberName always yields a value, and params must be at the end of the parameter list",
+    Scope = "member", Target = "~T:Reception.App.ViewModels.MainViewModel.ShowErrorAction")]
